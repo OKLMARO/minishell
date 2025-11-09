9752a51ee05a4f8db552242d0f0c60a6b57e07af
.Omar <55897834+OKLMARO@users.noreply.github.com>
2025-11-09 19:16:09 +0000

    sync

--- minishell.c
+++ minishell.c
@@ -344,65 +344,62 @@ int main(int argc, char **argv, char **env)
 		i = 0;
 		while (args[i])
 		{
-			if (is_token(token, args[i]))
+			/*if (ft_strncmp(is_token(token, args[i]), "|", 3))
+				pipex(...);*/
+			if (ft_strncmp(args[i], "<", 1) == 0)
 			{
-				/*if (ft_strncmp(is_token(token, args[i]), "|", 3))
-					pipex(...);*/
-				if (ft_strncmp(is_token(token, args[i]), "<", 1) == 0)
-				{
-					if (redirect_in(args, i, token) == 1)
-						break;
-				}
-				else if (ft_strncmp(is_token(token, args[i]), ">", 1) == 0)
-				{
-					if (redirect_out(args, i, token) == 1)
-						break;
-				}
-				/*else if (ft_strncmp(is_token(token, args[i]), "<<", 3))
-					jsp(...);
-				else if (ft_strncmp(is_token(token, args[i]), ">>", 3))
-					redirect_out_append(...);*/
+				if (redirect_in(args, i, token) == 1)
+					break;
 			}
-			i++;
-		}
-		if (args[0] && ft_strncmp(input, "pwd", 4) == 0)
-			builtin_pwd();
-		else if (args[0] && ft_strncmp(args[0], "cd", 3) == 0)
-		{
-			if (builtin_cd(args, env) == -1)
-				return (1);
-		}
-		else if (args[0] && ft_strncmp(input, "echo", 5) == 0)
-			builtin_echo(args);
-		else if (args[0])
-		{
-			pid = fork();
-			if (pid == 0)
+			else if (ft_strncmp(args[i], ">", 1) == 0)
 			{
-				if (!make_storage(&cmd, input, &all_cmd, path))
+				if (redirect_out(args, i, token) == 1)
+					break;
+			}
+			/*else if (ft_strncmp(is_token(token, args[i]), "<<", 3))
+				jsp(...);
+			else if (ft_strncmp(is_token(token, args[i]), ">>", 3))
+				redirect_out_append(...);*/
+			else if (args[i] && ft_strncmp(args[i], "pwd", 4) == 0)
+				builtin_pwd();
+			else if (args[i] && ft_strncmp(args[i], "cd", 3) == 0)
+			{
+				if (builtin_cd(args, env) == -1)
+					return (1);
+			}
+			else if (args[i] && ft_strncmp(args[i], "echo", 5) == 0)
+				builtin_echo(args);
+			else if (args[i])
+			{
+				pid = fork();
+				if (pid == 0)
 				{
-					signal(SIGINT, SIG_DFL);
-					signal(SIGQUIT, SIG_DFL);
-					if (execve(all_cmd, cmd, env) == -1)
+					if (!make_storage(&cmd, input, &all_cmd, path))
 					{
-						ft_putstr_fd("exec error\n", 2);
-						return (1);
+						signal(SIGINT, SIG_DFL);
+						signal(SIGQUIT, SIG_DFL);
+						if (execve(all_cmd, cmd, env) == -1)
+						{
+							ft_putstr_fd("exec error\n", 2);
+							return (1);
+						}
 					}
 				}
+				else if (pid > 0)
+				{
+					signal(SIGQUIT, sigquit_handler2);
+					signal(SIGINT, sigint_handler2);
+					waitpid(pid, NULL, 0);
+					signal(SIGQUIT, SIG_IGN);
+					signal(SIGINT, sigint_handler);
+				}
+				else
+				{
+					ft_putstr_fd("fork error", 2);
+					return (1);
+				}
 			}
-			else if (pid > 0)
-			{
-				signal(SIGQUIT, sigquit_handler2);
-				signal(SIGINT, sigint_handler2);
-				waitpid(pid, NULL, 0);
-				signal(SIGQUIT, SIG_IGN);
-				signal(SIGINT, sigint_handler);
-			}
-			else
-			{
-				ft_putstr_fd("fork error", 2);
-				return (1);
-			}
+			i++;
 		}
 		free(input);
 		free_double(args);
