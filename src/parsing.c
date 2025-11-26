f706d10f90bfac2f8a91a7b47b30e84c08253421
.Omar <55897834+OKLMARO@users.noreply.github.com>
2025-11-26 18:13:19 +0100

    Sync lexer

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: oamairi <oamairi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 10:46:30 by oamairi           #+#    #+#             */
-/*   Updated: 2025/11/24 16:21:02 by oamairi          ###   ########.fr       */
+/*   Updated: 2025/11/26 18:09:41 by oamairi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,20 +47,22 @@ bool	lexer_ter(char *input, t_token *list, int *i, int *i_buffer)
 		lexer_single_quote(ft_strdup(&input[*i]));
 		if (*i_buffer != 0)
 			lexer_word();
-		return (*i_buffer = 0, false);
+		return (*i_buffer = 0, true);
 	}
 	else if (input[*i] == '"')
 	{
 		lexer_double_quote(ft_strdup(&input[*i]));
 		if (*i_buffer != 0)
 			lexer_word();
-		*i_buffer = 0;
+		return (*i_buffer = 0, true);
 	}
 	else if (input[*i] == ' ')
 	{
 		if (*i_buffer != 0)
 			lexer_word();
+		return (*i_buffer = 0, true);
 	}
+	return (false);
 }
 
 bool	lexer_bis(char *input, t_token *list, int *i, int *i_buffer)
@@ -70,44 +72,48 @@ bool	lexer_bis(char *input, t_token *list, int *i, int *i_buffer)
 		lexer_redirect_out(ft_strdup(&input[*i]));
 		if (*i_buffer != 0)
 			lexer_word();
-		*i_buffer = 0;
+		return (*i_buffer = 0, true);
 	}
 	else if (input[*i] == '|')
 	{
 		lexer_pipe(ft_strdup(&input[*i]));
 		if (*i_buffer != 0)
 			lexer_word();
-		*i_buffer = 0;
+		return (*i_buffer = 0, true);
 	}
 	else if (input[*i] == '$')
 	{
 		lexer_env_var(ft_strdup(&input[*i]));
 		if (*i_buffer != 0)
 			lexer_word();
-		*i_buffer = 0;
+		return (*i_buffer = 0, true);
 	}
+	return (false);
 }
 
 t_token	*lexer(char *input)
 {
-	int		*i;
+	int		i;
 	t_token	*list;
 	char	buffer[4096];
-	int		*i_buffer;
+	int		i_buffer;
 
-	*i = 0;
-	*i_buffer = 0;
-	while (input[*i])
+	i = 0;
+	i_buffer = 0;
+	while (input[i])
 	{
-		if (lexer_quad(input, list, i, i_buffer) == false);
-		lexer_bis(input, list, i, i_buffer);
-		lexer_ter(input, list, i, i_buffer);
-		if (input != ' ')
+		if (lexer_quad(input, list, &i, &i_buffer) == false)
 		{
-			buffer[*i_buffer] = input[*i];
-			*i_buffer++;
+			if (lexer_bis(input, list, &i, &i_buffer) == false)
+			{
+				if (lexer_ter(input, list, &i, &i_buffer) == false)
+				{
+					buffer[i_buffer] = input[i];
+					i_buffer++;
+				}
+			}
 		}
-		*i++;
+		i++;
 	}
 	return (list);
 }
